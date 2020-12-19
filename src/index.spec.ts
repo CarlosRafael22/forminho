45ec465e8cf45c697aef29ef018055ac648960a7
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-19 08:01:09 -0300

    test(index): Add test to check exported components

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import {Form, InputField} from './';
+
+
+test('It should export Form and InputField', () => {
+    expect(Form).toBeDefined();
+    expect(InputField).toBeDefined();
+});
--- src/index.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import ExampleComponent from "./";
-
-test("create a new hello", () => {
-  const { getByText } = render(<ExampleComponent text={'Mundo'} />)
-  expect(getByText('Hello Mundo')).not.toBeNull();
-});

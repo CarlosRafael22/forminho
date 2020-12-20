285a3c573cc948ddd8c9fcc819d35d17e2791854
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-20 19:59:58 -0300

    feat(stories): Add Button stories

---
+++ src/stories/button/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Button from '../../button';
+
+export default {
+    title: 'Example/Button',
+    component: Button
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Custom = Template.bind({});
+Custom.args = {
+    text: 'Send',
+    style: {
+        backgroundColor: 'black',
+        color: 'white'
+    }
+};

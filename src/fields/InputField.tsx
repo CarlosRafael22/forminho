0cb4975a261e0aa1ee89027d9e67cfdfbebd446d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-20 19:56:34 -0300

    style(inputfield): Add InputDiv styled component

--- src/fields/InputField.tsx
+++ src/fields/InputField.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { InputDiv } from './styles';
 
 const InputField = ({type, name, value, placeholder, onChange}: InputFieldProps) => (
     <div className="form-group">
         {/* <label htmlFor={`${name}-input`}>{label}</label> */}
-        <input name={name} type={type} className="form-control" id={name} aria-describedby={`${name}-help`} aria-label={name}
+        <InputDiv name={name} type={type} id={name} aria-describedby={`${name}-help`} aria-label={name}
             placeholder={placeholder} value={value} onChange={onChange} />
             {/* placeholder={placeholder} value={value} onChange={(event) => onChange(event.target.value)} /> */}
         {/* {helpText && <small id={`${name}-help`} className="form-text text-muted" data-testid='helpText'>{helpText}</small>} */}
---
+++ src/fields/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const InputDiv = styled.input`
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 2em;
+    display: inline-block;
+    line-height: 1;
+    color: black;
+    background-color: #eeeeee;
+    font-size: 12px;
+    padding: 10px 16px;
+`;

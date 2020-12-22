040efb41140ca93318d58cdfd08b7823f55da60a
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-21 21:41:08 -0300

    feat(stories): Create Alert stories

---
+++ src/stories/alert/Alert.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Alert from '../../alert';
+
+
+export default {
+    title: 'Example/Alert',
+    component: Alert
+} as Meta;
+
+const Template: Story<AlertProps> = (args) => <Alert {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+    text: 'An error occurred'
+};
+
+export const CustomStyle = Template.bind({});
+CustomStyle.args = {
+    text: 'An error occurred',
+    style: { backgroundColor: 'red', color: 'white' }
+};

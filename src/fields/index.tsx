c0415457c21b8475741edf4d6ff0578c806bd94d
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-24 09:04:45 -0300

    feat(field): Create Field export

---
+++ src/fields/index.tsx
@@ -0,0 +1,11 @@
+import InputField from './input/InputField';
+import TextArea from './textarea';
+import Select from './select';
+
+const Field = {
+    Input: InputField,
+    TextArea,
+    Select
+};
+
+export default Field;

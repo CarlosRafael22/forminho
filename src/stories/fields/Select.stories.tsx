b7d678db2c81e21b2ed65bcd1817d15c759dde87
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-24 09:08:32 -0300

    feat(stories): Add Select stories

---
+++ src/stories/fields/Select.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Field from '../../fields';
+
+export default {
+    title: 'Simple-Form/Select',
+    component: Field.Select
+} as Meta;
+
+const Template: Story<SelectProps> = (args) => (
+    <Field.Select {...args} >
+        <option value='Chelsea'>Chelsea</option>
+        <option value='Arsenal'>Arsenal</option>
+        <option value='Tottenham'>Tottenham</option>
+        <option value='West Ham'>West Ham</option>
+    </Field.Select>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    name: 'team',
+    value: 'Arsenal'
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+    name: 'team',
+    value: 'Arsenal',
+    label: 'Select the best club in London'
+};

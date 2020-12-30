4577f55954f5e2ab2f67d35fb5e624c7dac252e3
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-12-30 18:46:13 -0300

    feat(index): Export handleFieldError

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import Form from './form/form';
+import Form, { handleFieldError } from './form/form';
 import Field from './fields/field';
 import Button from './button';
 
 export {
   Form,
   Field,
-  Button
+  Button,
+  handleFieldError
 };

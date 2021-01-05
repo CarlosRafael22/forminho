04a54266ee01c5a385512a5595e2a10384bbe8d1
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-05 12:56:57 -0300

    feat(livevalue): Change ref to useCallback and attach as array to context.liveValuesRefs

--- src/fields/LiveValue.tsx
+++ src/fields/LiveValue.tsx
@@ -1,29 +1,30 @@
-import React, { useRef, useContext, useEffect } from "react";
+import React, { useRef, useContext, useCallback } from "react";
 import { FormContext, FormContextType } from '../Forminho';
 
 const LiveValue = (props: any) => {
     const spanRef = useRef(null);
     console.log("LiveValue: ", props, props.fieldName);
-    // const currentValue = props.name?.current?.value;
-    // console.log("value: ", currentValue);
-
     const context = useContext(FormContext) as FormContextType;
     // console.log("Context in the LiveValue ", context, context.formRef.current);
+    const ref = useCallback((node) => {
+        console.log('Attached liveValue: ', node)
+        if (node) {
+          if (context.liveValuesRefs[props.fieldName]) {
+            console.log("here");
+            context.liveValuesRefs[props.fieldName].push(node);
+          } else {
+            console.log("there");
+            context.liveValuesRefs[props.fieldName] = [node];
+            // context.liveValues[props.fieldName] = Array(0).fill(spanRef);
+            // context.liveValues[props.fieldName] = [spanRef];
+          }
+        }
+      }, [props.fieldName, context.liveValuesRefs]);
 
-    useEffect(() => {
-        console.log(
-        "Effect LiveValue: ",
-        // currentValue,
-        context.formRef.current,
-        spanRef
-        );
-    });
-
-    context.liveValuesRefs[props.fieldName] = spanRef;
+    // context.liveValuesRefs[props.fieldName] = spanRef;
     console.log("Ref in the LiveValue: ", spanRef, context.liveValuesRefs);
 
-    // return <span ref={props.optRef}></span>;
-    return <span ref={spanRef}></span>;
+    return <span data-testid='livevalue' ref={ref}></span>;
 };
 
 export default LiveValue;

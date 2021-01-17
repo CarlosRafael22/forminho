527b392e77af55feb812bcabe4b97765d7aed21f
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-17 16:27:58 -0300

    chore(stories): Remore type field from Radio.stories

--- src/stories/fields/Radio.stories.tsx
+++ src/stories/fields/Radio.stories.tsx
@@ -12,7 +12,6 @@ const Template: Story<RadioProps> = (args) => (<Field.Radio {...args} />);
 
 export const Default = Template.bind({});
 Default.args = {
-    type: 'radio',
     name: 'team',
     value: 'Chelsea',
     label: 'Chelsea'

972d1c53cd72d239a9db074300a9df931dc9f0ff
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-20 19:09:33 -0300

    feat(button): Add children to component and rRemove styles file to use inline defaultStyle object

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import { ButtonDiv } from './styles';
+import React, { CSSProperties } from 'react';
 
-const Button = ({text = 'Submit', style = {}}: ButtonProps) => (
-    <ButtonDiv style={style}>{text}</ButtonDiv>
-);
+const Button = ({text = 'Submit', style = {}, children}: ButtonProps) => {
+
+    return (
+        <button style={{...defaultStyle, ...style}}>{children ? children : text}</button>
+    )
+}
+
+const defaultStyle = {
+    fontFamily: "'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    fontWeight: 700,
+    border: '0',
+    borderRadius: '3em',
+    cursor: 'pointer',
+    display: 'inline-block',
+    lineHeight: 1,
+    color: 'white',
+    backgroundColor: '#1ea7fd',
+    fontSize: '12px',
+    padding: '10px 16px',
+    marginTop: '0.5rem',
+} as CSSProperties
 
 export default Button;

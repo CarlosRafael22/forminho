6546ea155d7d311d327b43358020e1e4dfe32a45
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-20 20:37:00 -0300

    test(button): Add Css rendering case

--- src/button/button.spec.tsx
+++ src/button/button.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Button from './'
-import { Custom as CustomButton } from './Button.stories'
+import { Custom as CustomButton, WithCss } from './Button.stories'
 
 
 describe('Render Button', () => {
@@ -23,5 +23,26 @@ describe('Render Button', () => {
         }
         render(<Button {...props} />)
         expect(screen.getByRole('button', { name: /Hello/i })).toBeInTheDocument()
+    })
+
+    it('should render with css style if prop was passed', () => {
+        render(<WithCss {...WithCss.args} />)
+        const button = screen.getByRole('button', { name: WithCss.args.text })
+        screen.debug()
+        expect(button).toBeInTheDocument()
+        expect(button).toHaveStyle({
+            backgroundColor: 'red',
+            color: 'white',
+            fontSize: '16px'
+        })
+
+        // NEEDS TO TEST WHETHER ON HOVER IT CHANGES STYLE
+        // fireEvent.mouseOver(button)
+        // fireEvent.mouseEnter(button)
+        // expect(button).toHaveStyle({
+        //     backgroundColor: 'blue',
+        //     color: 'white',
+        //     fontSize: '16px'
+        // })
     })
 })

014bfa56b71d1fda9586be34874647e84614d7e8
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-26 20:02:43 -0300

    feat(type): Add StyleProps to FormProps

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,23 +10,10 @@ type ObjectType = {
     [key: string]: any
 }
 
-// type FormHandlerHookType = {
-//     initialValues: initialValuesType,
-//     onSubmitHandler: Function,
-//     onValidationHandler?: (values: initialValuesType) => void
-// }
-
-// type FormHandlerHookReturn = {
-//     values: initialValuesType,
-//     onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void,
-//     submitHandler: (event: React.FormEvent) => void,
-//     error?: string
-// }
-
-interface FormProps {
+type FormProps = StyleProps & {
     onSubmitHandler: (currentValues: ObjectType) => void,
     initialValues: ObjectType,
-    children?: Array<React.ReactElement>,
+    children?: Array<React.ReactNode>,
     onChangeHandler?: (event: React.ChangeEvent<HTMLFormElement>, currentValues: ObjectType) => void,
     onLiveErrorFeedback?: (currentValues: ObjectType, context: FormContextType) => void,
     onValidationHandler?: (values: ObjectType) => void,

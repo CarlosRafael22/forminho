e4be19e815fdb775033d4db13c05d465f2b185f9
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-03-18 11:35:33 -0300

    chore(types): Remove withLiveValues from FormProps

--- src/types.d.ts
+++ src/types.d.ts
@@ -30,8 +30,7 @@ interface FormProps {
     onChangeHandler?: (event: React.ChangeEvent<HTMLFormElement>, currentValues: ObjectType) => void,
     onLiveErrorFeedback?: (currentValues: ObjectType, context: FormContextType) => void,
     onValidationHandler?: (values: ObjectType) => void,
-    submitButtonText?: string,
-    withLiveValues?: string[]
+    submitButtonText?: string
 }
 
 type GenericInputChangeEvent = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>;

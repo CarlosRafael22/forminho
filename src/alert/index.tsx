35112f538247a28d2b24751e9b1b8b1800837f3e
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-03-18 20:29:31 -0300

    feat(alert): Refactor style to object and create story file

--- src/alert/index.tsx
+++ src/alert/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import { StyledAlert } from './styles';
+import React, { CSSProperties } from 'react'
+
+const defaultStyle = {
+    position: 'relative',
+    padding: '.25rem 1rem',
+    marginBottom: '1rem',
+    border: '1px solid transparent',
+    borderRadius: '.25rem',
+
+    color: '#721c24',
+    backgroundColor: '#f8d7da',
+    borderColor: '#f5c6cb',
+} as CSSProperties
 
 const Alert = ({text, style}: AlertProps) => (
-    <StyledAlert style={style} role="alert">{text}</StyledAlert>
-);
+    <div style={{...defaultStyle, ...style}} role="alert">{text}</div>
+)
 
-export default Alert;
+export default Alert

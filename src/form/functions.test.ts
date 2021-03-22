8ddf1896f5e7245c932e22941c71db9a24665445
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-03-22 09:19:10 -0300

    test(form): Create initial tests for fieldValidator

--- src/form/functions.test.ts
+++ src/form/functions.test.ts
@@ -1,34 +1,64 @@
 // import { getRefKeys } from '.';
-import { getUpdatedLiveValuesFromRefs, checkHasFilledValues } from './utils'
+// import { getUpdatedLiveValuesFromRefs, checkHasFilledValues, fieldValidator } from './utils'
+import { fieldValidator } from './utils'
 
-describe('Test getUpdatedLiveValues', () => {
-    it('should return correct values', () => {
-        const values = {firstName: "", team: ""}
-        const refValues = {firstName: "JonhA", lastName: "Doe", team: "Arsenal", text: "", agreed: false }
+// describe('Test getUpdatedLiveValues', () => {
+//     it('should return correct values', () => {
+//         const values = {firstName: "", team: ""}
+//         const refValues = {firstName: "JonhA", lastName: "Doe", team: "Arsenal", text: "", agreed: false }
 
-        const updatedLiveValues = getUpdatedLiveValuesFromRefs(values, refValues)
-        expect(updatedLiveValues).toEqual({firstName: "JonhA", team: "Arsenal"})
-    })
-})
+//         const updatedLiveValues = getUpdatedLiveValuesFromRefs(values, refValues)
+//         expect(updatedLiveValues).toEqual({firstName: "JonhA", team: "Arsenal"})
+//     })
+// })
 
-describe('Test checkHasFilledValues', () => {
-    const tests = [
-        [{firstName: "", team: ""}, {firstName: "JonhA", team: "Arsenal" }, true],
-        [{firstName: "", team: ""}, {firstName: "", team: "Arsenal" }, true],
-        [{firstName: "", team: ""}, {firstName: "", team: "" }, false],
-        [{firstName: "", checked: false}, {firstName: "", checked: false }, true],
-        [{firstName: "", teams: []}, {firstName: "", teams: [] }, false],
-        [{firstName: "Jonh", teams: []}, {firstName: "", teams: [] }, false],
-        [{firstName: "", teams: []}, {firstName: "", teams: ["Chelsea"] }, true],
-    ]
+// describe('Test checkHasFilledValues', () => {
+//     const tests = [
+//         [{firstName: "", team: ""}, {firstName: "JonhA", team: "Arsenal" }, true],
+//         [{firstName: "", team: ""}, {firstName: "", team: "Arsenal" }, true],
+//         [{firstName: "", team: ""}, {firstName: "", team: "" }, false],
+//         [{firstName: "", checked: false}, {firstName: "", checked: false }, true],
+//         [{firstName: "", teams: []}, {firstName: "", teams: [] }, false],
+//         [{firstName: "Jonh", teams: []}, {firstName: "", teams: [] }, false],
+//         [{firstName: "", teams: []}, {firstName: "", teams: ["Chelsea"] }, true],
+//     ]
 
-    tests.forEach(([initial, final, expectedResult], index) => {
-        it(`should return correct values for case ${index}`, () => {
-            const hasFilled = checkHasFilledValues(final, initial)
-            expect(hasFilled).toBe(expectedResult)
-        })
-    })
+//     tests.forEach(([initial, final, expectedResult], index) => {
+//         it(`should return correct values for case ${index}`, () => {
+//             const hasFilled = checkHasFilledValues(final, initial)
+//             expect(hasFilled).toBe(expectedResult)
+//         })
+//     })
         
+// })
+
+describe('Test fieldValidator', () => {
+    it('should not pass on min()', () => {
+        const mockedSet = jest.fn()
+        const mockedClear = jest.fn()
+
+        fieldValidator('test', 'firstName', mockedSet, mockedClear).min(6, 'Should have more than 6 letters').max(10, 'Should have less than 6 letters')
+        expect(mockedSet).toHaveBeenCalledWith('firstName', 'Should have more than 6 letters')
+        expect(mockedClear).not.toHaveBeenCalled()
+    })
+
+    it('should pass on min() but not on max()', () => {
+        const mockedSet = jest.fn()
+        const mockedClear = jest.fn()
+
+        fieldValidator('testinho name', 'firstName', mockedSet, mockedClear).min(6, 'Should have more than 6 letters').max(10, 'Should have less than 10 letters')
+        expect(mockedSet).toHaveBeenCalledWith('firstName', 'Should have less than 10 letters')
+        expect(mockedClear).not.toHaveBeenCalled()
+    })
+
+    it('should not pass on required()', () => {
+        const mockedSet = jest.fn()
+        const mockedClear = jest.fn()
+
+        fieldValidator('', 'firstName', mockedSet, mockedClear).required().min(6, 'Should have more than 6 letters').max(10, 'Should have less than 10 letters')
+        expect(mockedSet).toHaveBeenCalledWith('firstName', 'firstName should be provided')
+        expect(mockedClear).not.toHaveBeenCalled()
+    })
 })
 
 // describe('Test functions from Forms', () => {
